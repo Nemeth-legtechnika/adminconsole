4b8e7e129ac5a35cb8b5876bbd59629c3398c7ff
Németh Botond <nemethbotond.sopron@gmail.com>
2023-04-10 17:23:41 +0200

    #8: Create TestController
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

---
+++ src/main/kotlin/com/nemethlegtechnika/adminconsole/controller/TestController.kt
@@ -0,0 +1,13 @@
+package com.nemethlegtechnika.adminconsole.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/test")
+    fun test(): ResponseEntity<String> {
+        return ResponseEntity.ok("Németh Légtechnika Kft.: Server is running.")
+    }
+}
