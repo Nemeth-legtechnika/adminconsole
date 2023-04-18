44de6827afe07ec1cac79879f71693ff0a2e5280
Németh Botond <73688652+Zsupi@users.noreply.github.com>
2023-04-19 01:05:32 +0200

    #16: Submodule (#17)
    * Move the main application into a submodule 
    ---
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

--- products/src/main/kotlin/com/nemethlegtechnika/products/controller/TestController.kt
+++ products/src/main/kotlin/com/nemethlegtechnika/products/controller/TestController.kt
@@ -1,4 +1,4 @@
-package com.nemethlegtechnika.adminconsole.controller
+package com.nemethlegtechnika.products.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,4 @@ pluginManagement {
 	}
 }
 rootProject.name = "adminconsole"
+include("products")
