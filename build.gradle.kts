5215b59a20debd474a657b58a6faab6ac3225001
Németh Botond <nemethbotond.sopron@gmail.com>
2025-04-03 22:25:02 +0200

    #65: Remove sonar from gradle
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ plugins {
 	id("org.springframework.boot")
 	id("io.spring.dependency-management")
 	id("org.jetbrains.kotlin.plugin.allopen")
-	id("org.sonarqube")
 }
 
 val jar: Jar by tasks
@@ -34,7 +33,6 @@ allprojects {
 		plugin("org.jetbrains.kotlin.jvm")
 		plugin("org.jetbrains.kotlin.plugin.spring")
 		plugin("org.jetbrains.kotlin.plugin.allopen")
-		plugin("org.sonarqube")
 	}
 
 	java.sourceCompatibility = JavaVersion.VERSION_17
@@ -56,20 +54,11 @@ allprojects {
 			annotation("jakarta.persistence.Entity")
 			annotation("jakarta.persistence.MappedSuperclass")
 		}
-
-		sonar {
-			properties {
-				property("sonar.projectKey", "Nemeth-legtechnika_adminconsole")
-				property("sonar.organization", "nemeth-legtechnika")
-				property("sonar.host.url", "https://sonarcloud.io")
-				property("sonar.coverage.jacoco.xmlReportPaths", "${this@allprojects.buildDir}/reports/kover/report.xml")
-			}
-		}
 	}
 
 	tasks {
 		withType<KotlinCompile> {
-			kotlinOptions {
+			kotlinOptionFs {
 				freeCompilerArgs = listOf("-Xjsr305=strict")
 				jvmTarget = "17"
 			}
