b500de882822ca42cf439d03089365d5021c2c22
Németh Botond <73688652+Zsupi@users.noreply.github.com>
2025-04-10 15:54:08 +0200

    Quickfix: Refactor gradle (#68)
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+    const val kotlin = "1.9.21"
+    const val springBoot = "3.2.0"
+    const val dependencyManagement = "1.1.4"
+    const val kover = "0.7.6"
+
+    const val mapstruct = "1.5.5.Final"
+    const val mockk = "1.13.4"
+    const val hibernateJpaModelGen = "6.4.0.Final"
+    const val dotenv = "4.0.0"
+    const val openapi = "2.3.0"
+    const val kotlinLogger = "5.1.0"
+    const val antlr = "4.10.1"
+    const val okta = "3.0.5"
+}
