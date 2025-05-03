602a9c1e1adf16c881ff4751516dd81f9a7ef92c
Németh Botond <73688652+Zsupi@users.noreply.github.com>
2025-05-03 09:26:14 +0200

    #67: Add keycloak without user management (#69)
    * #67: add keycloak without user management
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>
    
    * #67: Config keycloak
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>
    
    * #67: Keycloak configured to work in docker and localhost as well
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>
    
    * #67: Minor fixups
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>
    
    * #67: Minor fixups
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>
    
    ---------
    
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

--- products/src/main/kotlin/com/nemethlegtechnika/products/controller/TestController.kt
+++ products/src/main/kotlin/com/nemethlegtechnika/products/controller/TestController.kt
@@ -1,13 +1,29 @@
 package com.nemethlegtechnika.products.controller
 
+import com.nemethlegtechnika.products.dto.user.UserDto
+import com.nemethlegtechnika.products.dto.user.UserInfo
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
+@RequestMapping("api")
 class TestController {
     @GetMapping("/test")
     fun test(): ResponseEntity<String> {
         return ResponseEntity.ok("Németh Légtechnika Kft.: Server is running.")
+    }
+
+    @GetMapping("/me")
+    fun me(@AuthenticationPrincipal userInfo: UserInfo): ResponseEntity<UserDto> {
+        val response = UserDto(
+            username = userInfo.username,
+            email = userInfo.email ?: "",
+            roles = userInfo.roles
+        )
+        return ResponseEntity.ok(response)
     }
 }
